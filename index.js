c554430da5b02c5c83d6637f757e87ad2921a961
Ulisses Dias <ulisses@ft.unicamp.br>
2021-05-17 10:59:34 -0300

    Inserindo o array de mensagens

--- index.js
+++ index.js
@@ -14,5 +14,31 @@ app.get('/',
     }
 );
 
+app.get('/hello',
+function (req, res){    
+    res.send("Hello de Novo");
+    }
+)
 
+const mensagens = [
+    "Elziele da Rocha", "Lucas Canova dos Santos"
+];
 
+app.get('/mensagens',
+    function(req, res){
+        res.send(mensagens);
+    }
+);
+
+app.get('/mensagens/:id',
+    function(req, res){
+        const id = req.params.id - 1;
+        const mensagem = mensagens[id];
+
+        if (!mensagem){
+            res.send("Mensagem não encontrada");
+        } else {
+            res.send(mensagem);
+        }
+    }
+)
