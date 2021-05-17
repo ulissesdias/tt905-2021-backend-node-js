faffdbbbebee8279130bb9b7d1e0d38df558657b
Ulisses Dias <ulisses@ft.unicamp.br>
2021-05-17 10:42:32 -0300

    Adicionando um endpoint

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//
+const express = require("express");
+const app = express();
+
+// Permissões
+var cors = require('cors');
+app.use(cors());
+
+// Porta que eu estou ouvindo
+app.listen(3000);
+
+app.get('/', 
+    function (req, res){    
+        res.send("Hello World");
+    }
+);
+
+
+
