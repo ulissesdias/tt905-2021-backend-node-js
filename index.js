ab96ec8e7403778f4eebe4846c6e6db2db99c66a
Ulisses Martins Dias <ulisses@Ulissess-MacBook-Air.local>
2023-05-15 13:51:21 -0300

    modificando para rail

--- index.js
+++ index.js
@@ -1,180 +1,69 @@
-const express = require("express");
+const express = require('express');
+//const cors = require("cors")
 const app = express();
+
 app.use(express.json());
+//app.use(cors());
+app.listen(3000);
 
-// Permissões
-var cors = require('cors');
-app.use(cors());
 
-// Porta que eu estou ouvindo
-app.listen(process.env.PORT || 3000);
-
-app.get('/', 
-    function (req, res){    
-        res.send("Hello World");
-    }
-);
-
-app.get('/hello',
-function (req, res){    
-    res.send("Hello de Novo");
-    }
-)
-
-const mensagens = [
-    "Elziele da Rocha", "Lucas Canova dos Santos", 0 
-];
-
-app.get('/mensagens',
-    function(req, res){
-        // res.send(mensagens);
-        res.send(mensagens.filter(Boolean));
-    }
-);
-
-app.get('/mensagens/:id',
-    function(req, res){
-        const id = req.params.id - 1;
-        const mensagem = mensagens[id];
-
-        if (!mensagem){
-            res.send("Mensagem não encontrada");
-        } else {
-            res.send(mensagem);
-        }
-    }
-)
-
-app.post('/mensagens', 
-    (req, res) => {
-        console.log(req.body.mensagem);
-        const mensagem = req.body.mensagem;
-        mensagens.push(mensagem);
-        res.send("criar uma mensagem.")
-    }
-);
-
-app.put('/mensagens/:id',
-    (req, res) => {
-        const id = req.params.id - 1;
-        const mensagem = req.body.mensagem;
-        mensagens[id] = mensagem;        
-        res.send("Mensagem atualizada com sucesso.")
-    }
-)
-
-app.delete('/mensagens/:id', 
-    (req, res) => {
-        const id = req.params.id - 1;
-        delete mensagens[id];
-
-        res.send("Mensagem removida com sucesso");
-    }
-);
+app.get('/', function(req, res){res.send('Hello world')});
 
 /*
-  Daqui para baixo, uso o banco de dados MongoDB
+  Servidor propriamente dito
 */
 
-/*
-const mongodb = require('mongodb')
-const password = process.env.PASSWORD || "asdf";
-console.log(password);
+const notes = [
+    {id: 0, title: "Meu Primeiro Título", description : "Minha Primeira Descrição"},
+    {id: 1, title: "Meu Segundo Título", description : "Minha Segunda Descrição"}
+]
 
-const connectionString = `mongodb+srv://admin:${password}@cluster0.fhdc2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const endpoint = "/notes";
 
-const options = { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-};
-
-(async()=>{
-    const client = await mongodb.MongoClient.connect(connectionString, options);
-    const db = client.db('myFirstDatabase');
-    const mensagens = db.collection('mensagens');
-    console.log(await mensagens.find({}).toArray());
-
-    app.get('/database',
-        async function(req, res){
-        // res.send(mensagens);
-        res.send(await mensagens.find({}).toArray());
-    }
-);
-
-app.get('/database/:id',
-    async function(req, res){
-        const id = req.params.id;
-        const mensagem = await mensagens.findOne(
-            {_id : mongodb.ObjectID(id)}
-        );
-        console.log(mensagem);
-        if (!mensagem){
-            res.send("Mensagem não encontrada");
-        } else {
-            res.send(mensagem);
-        }
-    }
-);
-
-app.post('/database', 
-    async (req, res) => {
-        console.log(req.body);
-        const mensagem = req.body;
-        
-        delete mensagem["_id"];
-
-        mensagens.insertOne(mensagem);        
-        res.send("criar uma mensagem.");
-    }
-);
-
-app.put('/database/:id',
-    async (req, res) => {
-        const id = req.params.id;
-        const mensagem = req.body;
-
-        console.log(mensagem);
-
-        delete mensagem["_id"];
-
-        const num_mensagens = await mensagens.countDocuments({_id : mongodb.ObjectID(id)});
-
-        if (num_mensagens !== 1) {
-            res.send('Ocorreu um erro por conta do número de mensagens');
-            return;
-        }
-
-        await mensagens.updateOne(
-            {_id : mongodb.ObjectID(id)},
-            {$set : mensagem}
-        );
-        
-        res.send("Mensagem atualizada com sucesso.")
-    }
-)
-
-app.delete('/database/:id', 
-    async (req, res) => {
-        const id = req.params.id;
-        
-        await mensagens.deleteOne({_id : mongodb.ObjectID(id)});
-
-        res.send("Mensagem removida com sucesso");
-    }
-);
-
-})();
-
-*/
-
-/*
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:<password>@cluster0.fhdc2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+app.get(endpoint, function(req, res){
+    res.send(notes.filter(Boolean));
 });
-*/
+
+app.get(`${endpoint}/:id`, function(req, res){
+    const id = req.params.id;
+    const note = notes[id];
+
+    if (!note){
+        res.send("{}");
+    } else {
+        res.send(note);
+    }   
+});
+
+app.post(endpoint, (req, res) => {
+    const note = {
+        id : notes.length,
+        title : req.body["title"],
+        description : req.body["description"]
+    };
+    notes.push(note);
+    res.send("1");
+
+});
+
+app.put(`${endpoint}/:id`, (req, res) =>{
+    const id = parseInt(req.params.id);
+    const note = {
+        id : id,
+        title : req.body["title"],
+        description : req.body["description"]
+    };
+
+    notes[id] = note;
+    res.send("1");
+
+});
+
+app.delete(`${endpoint}/:id`, (req, res) => {
+    const id = req.params.id;
+    delete notes[id];
+    res.send("1");
+
+});
+
+
