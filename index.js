62d13dc058276ff634a96eaab080d1e4ea8b209f
Ulisses Martins Dias <ulisses@Ulissess-MacBook-Air.local>
2023-05-15 14:09:41 -0300

    Corrigindo porta

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 //app.use(cors());
-app.listen(3000);
+app.listen( process.env.PORT ||  3000);
 
 
 app.get('/', function(req, res){res.send('Hello world')});
